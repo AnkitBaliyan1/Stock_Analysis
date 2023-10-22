b4e288bc66e6be46e3d12cf9bba64af6046abb88
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-22 23:58:48 +0530

    updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,50 +4,62 @@ import streamlit as st
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-# user_input = 'AAPL'
-start = '2010-01-01'
-end = '2023-01-01'
-col = 'Adj Close'
-# n = 100
-
-
 def main():
+    # adding title to the page
     st.title("Stock Data Analysis")
-    st.subheader(f"Stock data from {start} - {end}")
 
     user_input = st.text_area("Enter the Stock", 'AAPL')
+    start = st.date_input("Enter start date")
+    end = st.date_input("Enter the end date")
 
-    ma_state = st.checkbox("MA")
-    if ma_state:
-        n = st.selectbox("Pick one", [50, 100, 200])
+    if start < end:
+        st.success("Awesome Choice! Let's start the Analysis now.")
+        st.subheader(f"Stock data from {start} - {end}")
 
-    st.write(" ")
-    # if st.button("Submit"):
-    df = yf.download(user_input, start, end)
-    # print(df.head())
+        ma_state = st.checkbox("MA")
+        if ma_state:
+            n = st.selectbox("Pick one", [50, 100, 200])
 
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(df[col], 'b', label='Stock price')
-    if ma_state:
-        df_ma = df[col].rolling(n).mean()
-        plt.plot(df_ma, 'r', label=f'MA{n}')
-    plt.title(f"Stock: {user_input} ({col})")
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.legend()
-    st.pyplot(fig)
+        st.write(" ")
+        # if st.button("Submit"):
+        df = yf.download(user_input, start, end)
+        # print(df.head())
 
-    log_state = st.checkbox("Log")
-    st.subheader("Volume in Trade")
-    fig2 = plt.figure(figsize=(12, 6))
-    if log_state:
-        plt.plot(np.log(df['Volume']), 'g', label='Volume Trade (log)')
+        ls_col = list(df.columns)[0:5]
+        col = st.selectbox("Select Columns for Analysis", ls_col)
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(df[col], 'b', label='Stock price')
+        if ma_state:
+            df_ma = df[col].rolling(n).mean()
+            plt.plot(df_ma, 'r', label=f'MA{n}')
+        plt.title(f"Stock: {user_input} ({col})")
+        plt.xlabel('Time')
+        plt.ylabel('Price')
+        plt.legend()
+        st.pyplot(fig)
+
+        st.subheader("Percentage Change")
+        dfpct = df[col].pct_change(periods=1)
+        fig = plt.figure(figsize=(12, 6))
+        plt.plot(dfpct, 'b')
+        plt.title(f"%age change in {col}")
+        st.pyplot(fig)
+
+        st.subheader("Volume in Trade")
+        log_state = st.checkbox("Log scale")
+        fig = plt.figure(figsize=(12, 6))
+        if log_state:
+            plt.plot(np.log(df['Volume']), 'g', label='Volume Trade (log)')
+        else:
+            plt.plot(df['Volume'], 'g', label='Volume Trade')
+        plt.legend()
+        st.pyplot(fig)
+
+        st.write("all done")
+    elif start == end:
+        st.warning("Start date is the same as the end date.")
     else:
-        plt.plot(df['Volume'], 'g', label='Volume Trade')
-    plt.legend()
-    st.pyplot(fig2)
-
-    st.subheader("               Thanks for giving it a shot..!!")
+        st.error("Start date is after the end date.")
 
 
 if __name__ == '__main__':
