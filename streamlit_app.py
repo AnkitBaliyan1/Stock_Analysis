e8486e762e1c1e8a370163e3fb2dd679881b05bc
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-22 22:57:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-# import yfinance as yf
+import yfinance as yf
 import matplotlib.pyplot as plt
 
 # user_input = 'AAPL'
@@ -15,6 +15,17 @@ def main():
     st.title("Stock Data Analysis")
     st.subheader(f"Stock data from {start} - {end}")
 
+    user_input = st.text_area("Enter the Stock", 'AAPL')
+
+    ma_state = st.checkbox("MA")
+    if ma_state:
+        n = st.selectbox("Pick one", [50, 100, 200])
+
+    st.write(" ")
+    # if st.button("Submit"):
+    df = yf.download(user_input, start, end)
+    # print(df.head())
+    st.write("all good here.")
 
 if __name__ == '__main__':
     main()
